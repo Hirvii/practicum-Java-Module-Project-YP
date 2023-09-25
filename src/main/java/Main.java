181cf17ac311ddc784c2bbbfd0ed6de291bc280f
Vlad <geoslavs@gmail.com>
2023-09-25 21:57:23 +0300

    Add method countGuests.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int count = countGuests(scanner);
+        System.out.println(count);
     }
+
+    public static int countGuests(Scanner scanner) {
+        System.out.println("Ведите количество гостей: ");
+
+        while (true) {
+            if(scanner.hasNextInt()) {
+                int count = scanner.nextInt();
+                    if (count == 1) {
+                        System.out.println("Нет необходимоти делить счёт.");
+                        return count;
+                    } else if (count < 1) {
+                        System.out.println("Число гостей должно быть больше 1!");
+                    } else {
+                        return count;
+                    }
+            } else {
+                System.out.println("Вы ввели некорректное число гостей! \n Повторите ввод: ");
+                scanner.next();
+            }
+        }
+    }
+
 }
+
+/*public class Calulator {
+
+}*/
