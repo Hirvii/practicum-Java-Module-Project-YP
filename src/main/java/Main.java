f2b317e13062575fc60ea1ee6e0841007c276316
Vlad <geoslavs@gmail.com>
2023-09-28 10:09:53 +0300

    Добавлена проверка случая с 1 гостем. Исправлено отображение слова рубль при стоимостях от 11 до 14.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,57 +7,61 @@ public class Main {
 
         Calulator calculator = new Calulator();
 
-        System.out.println("Введите название товара (или команду \"Завершить\", чтобы завершить работу приложения)");
+        if (guestCount != 1) {
 
-        while(true) {
+            System.out.println("Введите название товара (или команду \"Завершить\", чтобы завершить работу приложения)");
 
-            Scanner scanner = new Scanner(System.in);
-            scanner.useDelimiter("\n");
+            while (true) {
 
-            if (scanner.hasNextLine()) {
-                String itemName = scanner.nextLine();
+                Scanner scanner = new Scanner(System.in);
+                scanner.useDelimiter("\n");
 
-                if (itemName.equalsIgnoreCase("завершить")) {
-                    break;
-                } else if(itemName.equals("")) {
-                    System.out.println("Название товара не может быть пустым! Введите название товара еще раз: ");
-                } else {
-                    calculator.addItemName(itemName + "\n");
+                if (scanner.hasNextLine()) {
+                    String itemName = scanner.nextLine();
 
-                    System.out.println("Введите стоимость товара в формате рубли.копейки");
+                    if (itemName.equalsIgnoreCase("завершить")) {
+                        break;
+                    } else if (itemName.equals("")) {
+                        System.out.println("Название товара не может быть пустым! Введите название товара еще раз: ");
+                    } else {
+                        calculator.addItemName(itemName + "\n");
 
-                    while (true) {
-                        if (scanner.hasNextDouble()) {
-                            double itemCost = scanner.nextDouble();
+                        System.out.println("Введите стоимость товара в формате рубли.копейки");
 
-                            if (itemCost > 0) {
-                                calculator.addItemCost(itemCost);
+                        while (true) {
+                            if (scanner.hasNextDouble()) {
+                                double itemCost = scanner.nextDouble();
 
-                                System.out.println("Товар успешно добавлен\n");
-                                System.out.printf("Текущая общая сумма товаров: %.2f ", calculator.itemsCosts);
-                                System.out.println(getRuble(calculator.itemsCosts) + "\n");
-                                break;
+                                if (itemCost > 0) {
+                                    calculator.addItemCost(itemCost);
+
+                                    System.out.println("Товар успешно добавлен\n");
+                                    System.out.printf("Текущая общая сумма товаров: %.2f ", calculator.itemsCosts);
+                                    System.out.println(getRuble(calculator.itemsCosts) + "\n");
+                                    break;
+                                } else {
+                                    System.out.println("Стоимость не может быть отрицательной! Введите стоимость еще раз:");
+                                }
                             } else {
-                                System.out.println("Стоимость не может быть отрицательной! Введите стоимость еще раз:");
+                                System.out.println("Вы ввели не число!");
+                                scanner.next();
                             }
-                        } else {
-                            System.out.println("Вы ввели не число!");
-                            scanner.next();
                         }
                     }
+                    System.out.println("Введите название следующего товара (или команду \"Завершить\", если все товары добавлены)");
                 }
-                System.out.println("Введите название следующего товара (или команду \"Завершить\", если все товары добавлены)");
             }
+            System.out.println("Добавленные товары:\n" + calculator.itemsNames);
+            System.out.printf("Итоговая общая сумма товаров: %.2f ", calculator.itemsCosts);
+            System.out.println(getRuble(calculator.itemsCosts) + "\n");
+
+            double costPerGuest = calculator.itemsCosts / guestCount;
+
+            System.out.printf("Каждый гость должен заплатить: %.2f ", costPerGuest);
+            System.out.println(getRuble(costPerGuest));
+        } else {
+            System.out.println("Нет необходимоти делить счёт.");
         }
-        System.out.println("Добавленные товары:\n" + calculator.itemsNames);
-        System.out.printf("Итоговая общая сумма товаров: %.2f ", calculator.itemsCosts);
-        System.out.println(getRuble(calculator.itemsCosts) + "\n");
-
-        double costPerGuest = calculator.itemsCosts / guestCount;
-
-        System.out.printf("Каждый гость должен заплатить: %.2f ", costPerGuest);
-        System.out.println(getRuble(costPerGuest));
-
     }
 
     public static int countGuests(Scanner scanner) {
@@ -67,10 +71,7 @@ public class Main {
             if(scanner.hasNextInt()) {
                 int count = scanner.nextInt();
 
-                if (count == 1) {
-                    System.out.println("Нет необходимоти делить счёт. Вы можете добавить товары, чтобы посчитать их общую сумму.");
-                    return count;
-                } else if (count < 1) {
+                if (count < 1) {
                     System.out.println("Число гостей должно быть больше 1! Введите число гостей ещё раз: ");
                 } else {
                     return count;
@@ -84,9 +85,9 @@ public class Main {
     public static String getRuble(double cost) {
         int costInt = (int)cost;
 
-        if ((costInt == 1) || (costInt % 10 == 1)) {
+        if ((costInt == 1) || (costInt % 10 == 1 && costInt != 11)) {
             return "рубль";
-        } else if((costInt >= 2 && costInt <=4) || (costInt % 10 >= 2 && costInt % 10 <= 4)) {
+        } else if((costInt >= 2 && costInt <=4) || ((costInt % 10 >= 2 && costInt % 10 <= 4) && (costInt != 12 && costInt != 13 && costInt != 14))) {
             return "рубля";
         } else {
             return "рублей";
